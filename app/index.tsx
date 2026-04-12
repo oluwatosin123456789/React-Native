1eee3417c5d74b50408676b45f102a11b681717c
oluwatosin123456789 <jeremiahshadamoro@gmail.com>
2026-04-12 23:39:12 +0100

    shege

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-";
+
+export default function RootLayout() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
---
+++ app/login.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function LoginScreen() {
+  return (
+    <View>
+      {""}
+      <Text>Hello</Text>
+    </View>
+  );
+}
